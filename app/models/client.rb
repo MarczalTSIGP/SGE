72167e2fa8d12931e77c41149803a934896b8405
Anderson Lima <ander0807@gmail.com>
2018-11-12 14:08:14 -0200

    Correção de ofensa do rubocop

--- app/models/client.rb
+++ app/models/client.rb
@@ -21,7 +21,7 @@ class Client < ApplicationRecord
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
+    if (login = conditions.delete(:login))
       where(conditions.to_h).where(['lower(cpf) = :value OR lower(email) = :value',
                                     { value: login.downcase }]).first
     elsif conditions.haskey?(:cpf) || conditions.haskey?(:email)
