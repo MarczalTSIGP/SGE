128df8bd3d515c439c2770a9bf957ee6a45b145b
Anderson Lima <ander0807@gmail.com>
2019-11-22 14:00:26 -0300

    Correção

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,23 @@ User.create_with(name: 'Administrador',
                  password: '123456',
                  support: true).find_or_create_by(email: 'admin@utfpr.edu.br')
 
+User.create_with(name: 'Servidor',
+                 username: 'servidor',
+                 registration_number: '0000001',
+                 cpf: '76150313034',
+                 admin: false, active: true,
+                 password: '123456',
+                 support: true).find_or_create_by(email: 'servidor@utfpr.edu.br')
+
+Client.create_with(name: 'Participante',
+                   cpf: '36688844044',
+                   email: 'participante@gmail.com',
+                   kind: Client.kinds.values.sample,
+                   password: '123456').find_or_create_by(email: 'participante@gmail.com')
 roles = [
   ['Chefe de Departamento', :manager, true],
   ['Membro do Departamento', :member_department, true],
-  ['Responsavel da Divisão', :responsible, false],
+  ['Responsável da Divisão', :responsible, false],
   ['Membro da Divisão', :member_division, false]
 ]
 
